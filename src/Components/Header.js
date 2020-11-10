3e4a982c70a959c2b7f9b84c6fc10b5fa0f2925d
이승우 <starhn87@gmail.com>
2020-11-10 17:59:56 +0900

    헤더 부분 변경

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,25 +1,72 @@
 import React from 'react';
-import styles from 'styled-components';
+import { Link, withRouter } from 'react-router-dom';
+import styled from 'styled-components';
 
-const List = styles.ul`
-    display: flex;
-    &:hover{
-        background-color: blue;
-    }
+const Header = styled.header`
+	color: white;
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 50px;
+	display: flex;
+	align-items: center;
+	padding: 0 10px;
+	background-color: rgba(20, 20, 20, 0.8);
+	z-index: 10;
+	box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
 `;
 
-export default () => (
-    <header className={styles.navList}>
-        <List>
-            <li>
-                <a href='/'>Movies</a>
-            </li>
-            <li>
-                <a href='/tv'>TV</a>
-            </li>
-            <li>
-                <a href='/search'>Search</a>
-            </li>
-        </List>
-    </header>
-)
+const List = styled.ul`
+    display: flex;
+`;
+
+const Item = styled.li`
+	width: 50px;
+	text-align: center;
+	border-bottom: 5px solid ${props => (props.current ? '#3498db' : 'transparent')};
+	transition: border-bottom .5s ease-in-out;
+`;
+
+const SLink = styled(Link)`
+	height: 50px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+export default withRouter(({location: {pathname}}) => (
+	<Header>
+		<List>
+			<Item current={pathname === '/'}>
+				<SLink to='/'>Movies</SLink>
+			</Item>
+			<Item current={pathname === '/tv'}>
+				<SLink to='/tv'>TV</SLink>
+			</Item>
+			<Item current={pathname === '/search'}>
+				<SLink to='/search'>Search</SLink>
+			</Item>
+		</List>
+	</Header>
+));
+
+// 위의 것과 같은 기능을 한다
+// const HeaderC = (props) => (
+// 	<Header>
+// 		{console.log(props)}
+// 		<List>
+// 			<Item current={true}>
+// 				<SLink to='/'>Movies</SLink>
+// 			</Item>
+// 			<Item current={true}>
+// 				<SLink to='/tv'>TV</SLink>
+// 			</Item>
+// 			<Item current={true}>
+// 				<SLink to='/search'>Search</SLink>
+// 			</Item>
+// 		</List>
+// 	</Header>
+// );
+
+// export default withRouter(HeaderC);
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,15 +3,19 @@ import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom
 import Home from "../Routes/Home";
 import TV from "../Routes/TV";
 import Search from "../Routes/Search";
+import Header from './Header';
 
 export default () => (
     <Router>
-        <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/tv" component={TV} />
-            <Route path="/tv/popular" render={() => <h1>Popular</h1>} />
-            <Route path="/search" component={Search} />
-            <Redirect from='*' to={Home} />
-        </Switch>
+        <>
+            <Header />
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/tv" component={TV} />
+                <Route path="/tv/popular" render={() => <h1>Popular</h1>} />
+                <Route path="/search" component={Search} />
+                <Redirect from='*' to={Home} />
+            </Switch>
+        </>
     </Router>
 )
