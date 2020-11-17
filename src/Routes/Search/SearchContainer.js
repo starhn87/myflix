f9952d7a823b081f5cf2350cc0cff255f36ee0b4
이승우 <starhn87@gmail.com>
2020-11-17 17:53:53 +0900

    이미지 추가 및 배치 변경

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -7,15 +7,24 @@ export default class extends React.Component {
         movieResults: null,
         tvResults: null,
         searchTerm: "",
-        loading: false,
+        loading: true,
         error: null
     };
 
-    handleSubmit = () => {
+    handleSubmit = (event) => {
+        event.preventDefault();
         const { searchTerm } = this.state;
         if (searchTerm !== "") {
             this.searchByTerm();
         }
+    }
+
+    updateTerm = (event) => {
+        const {target: {value}} = event;
+        console.log(value);
+        this.setState({
+            searchTerm: value
+        });
     }
 
     searchByTerm = async () => {
@@ -43,6 +52,7 @@ export default class extends React.Component {
             loading={loading}
             error={error}
             handleSubmit={this.handleSubmit}
+            updateTerm={this.updateTerm}
         />
     }
 }
--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,6 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Loader from "../../Components/Loader";
+import Section from "../../Components/Section";
+import Message from "../../Components/Message";
+import Poster from "../../Components/Poster";
+
+const Container = styled.div`
+    padding: 0 20px;
+
+`;
+
+const Form = styled.form`
+    margin-bottom: 50px;
+`;
+
+const Input = styled.input`
+    all: unset;
+    width: 100%;
+    font-size: 28px;
+`;
 
 const SearchPresenter = ({
     movieResults,
@@ -8,8 +27,55 @@ const SearchPresenter = ({
     loading,
     error,
     searchTerm,
-    handleSubmit
-}) => null;
+    handleSubmit,
+    updateTerm
+}) => <Container>
+        <Form onSubmit={handleSubmit}>
+            <Input placeholder="Seach Movies or TV Shows..." value={searchTerm} onChange={updateTerm}>
+            </Input>
+        </Form>
+        {loading ? (
+            <Loader />
+        ) : (
+                <>
+                    {movieResults && movieResults.length > 0 && (
+                        <Section title="Movie Results">
+                            {movieResults.map(movie => (
+                                <Poster
+                                    key={movie.id}
+                                    id={movie.id}
+                                    imageUrl={movie.poster_path}
+                                    title={movie.original_title}
+                                    rating={movie.vote_average}
+                                    year={movie.release_date && movie.release_date.substring(0, 4)}
+                                    isMovie={true}
+                                />))}
+                            ))
+                        </Section>
+                    )}
+                    {tvResults && tvResults.length > 0 && (
+                        <Section title="TV Shows Results">
+                            {tvResults.map(show => (
+                                <Poster
+                                    key={show.id}
+                                    id={show.id}
+                                    imageUrl={show.poster_path}
+                                    title={show.original_name}
+                                    rating={show.vote_average}
+                                    year={show.first_air_date && show.first_air_date.substring(0, 4)}
+                                />))}
+                            ))
+                        </Section>
+                    )}
+                    {error && <Message color="#e74c3c" text={error} />}
+                    {tvResults
+                        && movieResults
+                        && tvResults.length === 0
+                        && movieResults.length === 0
+                        && <Message text="Nothing found" color="grey" />}
+                </>
+            )}
+    </Container>;
 
 SearchPresenter.propTypes = {
     movieResults: PropTypes.array,
@@ -17,7 +83,8 @@ SearchPresenter.propTypes = {
     loading: PropTypes.bool.isRequired,
     error: PropTypes.string,
     searchTerm: PropTypes.string,
-    handleSubmit: PropTypes.func.isRequired
+    handleSubmit: PropTypes.func.isRequired,
+    updateTerm: PropTypes.func.isRequired
 }
 
 export default SearchPresenter;
